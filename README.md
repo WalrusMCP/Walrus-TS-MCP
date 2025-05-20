cc0bc115cb529a0c9ba6e621bc72ad5913f40fd8
libaice <libaice147@gmail.com>
2025-05-20 15:35:12 +0800

    fix: update ts sdk

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Walrus MCP
+## Walrus MCP Tools
+
+Walrus TS MCP is a typescript MCP for Sui and Walrus protocol .
+
+
+### Installation & Configuration
+
+```
+{
+  "mcpServers": {
+    "sui-tools": {
+      "command": "npx",
+      "args": ["-y", "walrus-ts-sdk@latest"]
+    }
+  }
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
@@ -36,5 +38,14 @@ server.resource(
     })
 );
 
-const transport = new StdioServerTransport();
-await server.connect(transport);
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+
+}
+
+main().catch((error) => {
+    console.error("Error starting server:", error);
+    process.exit(1);
+}
+);
