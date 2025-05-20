76ee7aa0a3111fec02f62859707c6a8eaf88499b
libaice <libaice147@gmail.com>
2025-05-20 15:05:58 +0800

    add index.ts for sui price

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import fetch from "node-fetch";
+
+
+const server = new McpServer({
+    name: "Walrus TS MCP Server",
+    version: "1.0.0"
+});
+
+server.tool("get_sui_price", {}, async ({ }) => {
+    const sui_price = await fetchSuiPrice();
+    return {
+        content: [{ type: "text", text: String(sui_price) }]
+    };
+});
+
+
+async function fetchSuiPrice(): Promise<number | null> {
+    const url = "https://api.binance.com/api/v3/ticker/price?symbol=SUIUSDT";
+    const response = await fetch(url);
+    if (!response.ok) return null;
+    const data = await response.json() as any;
+    return data.price;
+}
+
+server.resource(
+    "greeting",
+    new ResourceTemplate("greeting://{name}", { list: undefined }),
+    async (uri, { name }) => ({
+        contents: [{
+            uri: uri.href,
+            text: `Hello, ${name}!`
+        }]
+    })
+);
+
+const transport = new StdioServerTransport();
+await server.connect(transport);
